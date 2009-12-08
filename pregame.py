837758ca711f1546e35884dee13a3f458c20a17b
Dave.Przybylo <Dave.Przybylo@e6ab138e-e119-11de-9097-f71ab900b11a>
2009-12-08 04:02:21 +0000

    added some sounds

--- pregame.py
+++ pregame.py
@@ -9,6 +9,11 @@ class Pregame(DirectObject):
         self.posit = 0
         self.accept('mouse1', self.positionMove)
         self.a = OnscreenImage(parent=render2d, image=os.path.join("image files", "Title-Screen.png"))
+        self.sound_title = loader.loadSfx(os.path.join("sound files", "Title Theme.mp3"))
+        self.sound_main = loader.loadSfx(os.path.join("sound files", "Main Theme.mp3"))
+        self.sound_title.play()
+        self.sound_title.setLoop(True)
+
 
         
     def positionMove(self):
@@ -23,5 +28,8 @@ class Pregame(DirectObject):
             self.c.destroy()
             self.d = OnscreenImage(parent=render2d, image=os.path.join("image files", "Intro-Screen.png"))
         elif self.posit == 4:
+            self.sound_title.stop()
+            self.sound_main.play()
+            self.sound_main.setLoop(True)
             self.d.destroy()
             w = main.World()
