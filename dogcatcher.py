24deb63561df7ab86208ffd996ba27f5ab946d3f
MrSuborbian88 <MrSuborbian88@e6ab138e-e119-11de-9097-f71ab900b11a>
2009-12-09 07:07:22 +0000

    Added Wall Collisions for Enemies

--- dogcatcher.py
+++ dogcatcher.py
@@ -19,9 +19,21 @@ import sys
 class DogCatcher(enemy.Enemy):
     def __init__(self, pos,file_loc):
         enemy.Enemy.__init__(self, pos, file_loc)
+        self._setup_catcher_collisions()
 
     def _load_models(self, pos):
         self._model = Actor("models/cthr", {"enemove":"models/cthr"})
         self._model.setScale(1 * settings.GLOBAL_SCALE)
         self._model.setPos(pos[0], pos[1], pos[2])
         self._model.reparentTo(render)
+    def _setup_catcher_collisions(self):
+        # Player collision
+        self._player_handler = CollisionHandlerQueue()
+        self._player = CollisionSphere(0, 0, 0, 3)
+        self._player_coll = CollisionNode('collision-with-player-dcatcher')
+        self._player_coll.addSolid(self._player)
+        self._player_coll.setFromCollideMask(BitMask32.bit(7))
+        self._player_coll.setIntoCollideMask(BitMask32.bit(7))
+        self._player_coll_path = self._model.attachNewNode(self._player_coll)
+        self._player_coll_path.show()
+        self._coll_trav.addCollider(self._player_coll_path, self._player_handler)
