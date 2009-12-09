7b9201101a4f075fac65560462261455ebaeca2d
Dave.Przybylo <Dave.Przybylo@e6ab138e-e119-11de-9097-f71ab900b11a>
2009-12-09 18:01:36 +0000

    box

--- trunk/breakblock.py
+++ trunk/breakblock.py
@@ -7,6 +7,10 @@ from direct.gui.DirectGui import *
 from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import *
 from direct.task import Task
+from direct.particles.Particles import Particles
+from direct.particles.ParticleEffect import ParticleEffect
+from pandac.PandaModules import BaseParticleEmitter,BaseParticleRenderer
+from pandac.PandaModules import PointParticleFactory,SpriteParticleRenderer
 import enemy
 
 import settings
@@ -17,24 +21,40 @@ import sys
 
 
 class BreakBlock(DirectObject):
-    def __init__(self, pos,room):
+    def __init__(self, pos, room, number):
         self._load_models(pos,room)
-        self._setup_catcher_collisions()
+        self.room = room
+        self.number = number
+        self.p = ParticleEffect()
+        self.position = pos
+        self.destroyed = False
 
     def _load_models(self, pos,room):
         self._model = loader.loadModel("models/breakblock")
-        self._model.setPos(pos[0], pos[1], pos[2])
-        self._model.reparentTo(room)
-    def _setup_catcher_collisions(self):
-        self._coll_trav = CollisionTraverser()
-        # Player collision
-        self._player_handler = CollisionHandlerQueue()
-        self._player = CollisionSphere(0, -5, 0, 3)
-        self._player_coll = CollisionNode('collision-breakblock')
-        self._player_coll.addSolid(self._player)
-        self._player_coll.setFromCollideMask(BitMask32.bit(7))
-        self._player_coll.setIntoCollideMask(BitMask32.bit(7))
-        self._player_coll_path = self._model.attachNewNode(self._player_coll)
-        self._player_coll_path.show()
-        self._coll_trav.addCollider(self._player_coll_path, self._player_handler)
+        self._model.setPos(pos)
+        self._model.setScale(50)
+        self._model.reparentTo(render)
+        
+    def getRoom(self):
+        return self.number
+    
+    def destroyBlock(self):
+        if not self.destroyed:
+            self._model.removeNode()
+            #self.loadParticleConfig()
+            self.destroyed = True
+        
+    def loadParticleConfig(self):
+        self.p.cleanup()
+        self.p = ParticleEffect()
+        self.p.loadConfig(os.path.join("models", "smokering.ptf"))   
+        self.p.accelerate(.25) 
+        self.p.setPos(self.position) 
+        self.p.reparentTo(render) 
+        self.p.setScale(50)  
+        self.p.start()
+        #taskMgr.doMethodLater(5, self.cleanParticles, 'Stop Particles')
+        
+    def cleanParticles(self, random):
+        self.p.softStop()
         
